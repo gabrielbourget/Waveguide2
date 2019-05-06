95de2f13fc0e497f93c6363b33e28b983f766215
Gabriel Bourget <gabrielbourget@gmail.com>
2019-05-06 19:27:44 -0400

    Refactored TwoSizeCardGallery

---
+++ client/src/Components/Galleries/TwoSizeCardGallery/TwoSizeCardGallery.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ClassNames from 'classnames';
+import { ThemeContext } from '../../../ThemeContext';
+
+import styles from './TwoSizeCardGallery.module.scss';
+
+class TwoSizeCardGallery extends React.Component {
+	render() {
+		const initObject = prepareComponent(this.context);
+
+		return (
+			<div className={ initObject. galleryClasses }>
+				{ this.props.children }
+			</div>
+		);
+	}
+}
+
+const prepareComponent = (context) => {
+	const themeClass = (context === 'dark') ? styles.darkTheme : styles.lightTheme;
+	const galleryClasses = ClassNames(styles.gallery, themeClass);
+
+	return { galleryClasses };
+};
+
+TwoSizeCardGallery.contextType = ThemeContext;
+export default TwoSizeCardGallery;
