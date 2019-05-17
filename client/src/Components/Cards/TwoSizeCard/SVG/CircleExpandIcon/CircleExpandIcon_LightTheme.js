eb2c9734376754674da3109b9a775d8bcdb4316b
Gabriel Bourget <gabrielbourget@gmail.com>
2019-05-17 14:56:00 -0400

    Fixed color issue.

--- client/src/Components/Cards/TwoSizeCard/SVG/CircleExpandIcon/CircleExpandIcon_LightTheme.js
+++ client/src/Components/Cards/TwoSizeCard/SVG/CircleExpandIcon/CircleExpandIcon_LightTheme.js
@@ -4,7 +4,7 @@ const CircleExpandIcon_LightTheme = () => (
 	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 83.28 83.28">
 		<g id="Layer_2" data-name="Layer 2">
 			<g id="Layer_1-2" data-name="Layer 1">
-				<circle cx="41.64" cy="41.64" r="38.64" fill="none" stroke="#fff" strokeMiterlimit="10" strokeWidth="6"/>
+				<circle cx="41.64" cy="41.64" r="38.64" fill="none" stroke="#000" strokeMiterlimit="10" strokeWidth="6"/>
 				<path className="cls-2" style={{ fill: '#000' }}d="M62.01 21.27l-28.64 7.68 20.97 20.96 7.67-28.64zM21.27 62.01l28.64-7.67-20.96-20.97-7.68 28.64z"/>
 			</g>
 		</g>
